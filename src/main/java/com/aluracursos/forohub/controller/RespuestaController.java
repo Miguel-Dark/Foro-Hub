089488152d8d3c739c0906d2690df254d9b70360
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-28 08:15:24 -0600

    feat: create Response controller with register and list endpoints

---
+++ src/main/java/com/aluracursos/forohub/controller/RespuestaController.java
@@ -0,0 +1,40 @@
+package com.aluracursos.forohub.controller;
+
+import com.aluracursos.forohub.domain.respuesta.Respuesta;
+import com.aluracursos.forohub.domain.topico.TopicoRepository;
+import com.aluracursos.forohub.domain.usuario.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/respuestas")
+public class RespuestaController {
+
+    @Autowired
+    private RespuestaRepository repository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    @Transactional
+    @PostMapping
+    public void registrar(@RequestBody @Valid DatosRegistroRespuesta datos){
+        var autor = usuarioRepository.getReferenceById(datos.idUsuario());
+        var topico = topicoRepository.getReferenceById(datos.idTopico());
+        var respuesta = new Respuesta(datos.mensaje(), autor, topico);
+
+        repository.save(respuesta);
+    }
+
+    @GetMapping
+    public Page<DatosListaRespuesta> listar(@PageableDefault(size=10, sort={"fechaCreacion"}) Pageable paginacion) {
+        return repository.findAll(paginacion).map(DatosListaRespuesta::new);
+    }
+}
