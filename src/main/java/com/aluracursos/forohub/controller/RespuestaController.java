81fa3f60e8668667503ffd77759545d53908a0c2
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-28 09:00:11 -0600

    feat: add Response DTO and repository

--- src/main/java/com/aluracursos/forohub/controller/RespuestaController.java
+++ src/main/java/com/aluracursos/forohub/controller/RespuestaController.java
@@ -1,6 +1,8 @@
 package com.aluracursos.forohub.controller;
 
+import com.aluracursos.forohub.domain.respuesta.DatosRegistroRespuesta;
 import com.aluracursos.forohub.domain.respuesta.Respuesta;
+import com.aluracursos.forohub.domain.respuesta.RespuestaRepository;
 import com.aluracursos.forohub.domain.topico.TopicoRepository;
 import com.aluracursos.forohub.domain.usuario.UsuarioRepository;
 import jakarta.validation.Valid;
