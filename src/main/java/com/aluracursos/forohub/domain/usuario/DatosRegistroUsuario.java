2a965a9605b791c57cf3f9ef490436bf3fe1ec99
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-28 02:05:08 -0600

    feat: add repository and DTO for Usuario management

---
+++ src/main/java/com/aluracursos/forohub/domain/usuario/DatosRegistroUsuario.java
@@ -0,0 +1,12 @@
+package com.aluracursos.forohub.domain.usuario;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DatosRegistroUsuario(
+        @NotBlank String nombre,
+        @NotBlank @Email String email,
+        @NotBlank String contrasena,
+        @NotNull Perfil perfil) {
+}
