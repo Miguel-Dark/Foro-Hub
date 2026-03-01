aacf1d4da32838f6567a7d4123d9ed6689b01cf1
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-28 20:38:50 -0600

    feat: add findByNombre method to PerfilRepository

--- src/main/java/com/aluracursos/forohub/domain/perfil/PerfilRepository.java
+++ src/main/java/com/aluracursos/forohub/domain/perfil/PerfilRepository.java
@@ -3,4 +3,5 @@ package com.aluracursos.forohub.domain.perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PerfilRepository extends JpaRepository<Perfil, Long> {
+    Perfil findByNombre(String nombre);
 }
