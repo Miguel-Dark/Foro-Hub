45b4e2d1aa689bfd269ce41d2471585d791386d9
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-03-02 20:07:17 -0600

    feat: implement professional POST method with 201 Created status and Location header

--- src/main/java/com/aluracursos/forohub/controller/CursoController.java
+++ src/main/java/com/aluracursos/forohub/controller/CursoController.java
@@ -4,6 +4,8 @@ import com.aluracursos.forohub.domain.curso.Curso;
 import com.aluracursos.forohub.domain.curso.CursoRepository;
 import com.aluracursos.forohub.domain.curso.DatosListaCurso;
 import com.aluracursos.forohub.domain.curso.DatosRegistroCurso;
+import com.aluracursos.forohub.domain.topico.DatosDetalleTopico;
+import com.aluracursos.forohub.domain.topico.Topico;
 import com.aluracursos.forohub.domain.topico.TopicoRepository;
 import com.aluracursos.forohub.domain.usuario.UsuarioRepository;
 import jakarta.validation.Valid;
@@ -11,8 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/cursos")
@@ -23,8 +27,11 @@ public class CursoController {
 
     @Transactional
     @PostMapping
-    public void registrar(@RequestBody @Valid DatosRegistroCurso datos){
-        cursoRepository.save(new Curso(datos));
+    public ResponseEntity registrar(@RequestBody @Valid DatosRegistroCurso datos,
+                                    UriComponentsBuilder uriComponentsBuilder){
+        var curso = cursoRepository.save(new Curso(datos));
+        var uri = uriComponentsBuilder.path("/cursos/{id}").buildAndExpand(curso.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DatosDetalleCurso(curso));
     }
 
     @GetMapping
