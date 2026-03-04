0506250592e08b3246330b91637828445bb3adec
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-03-04 02:53:29 -0600

    feat: update AutenticacionController to return JWT token and create DatosTokenJWT DTO

--- src/main/java/com/aluracursos/forohub/controller/AutenticacionController.java
+++ src/main/java/com/aluracursos/forohub/controller/AutenticacionController.java
@@ -1,6 +1,9 @@
 package com.aluracursos.forohub.controller;
 
 import com.aluracursos.forohub.domain.usuario.DatosAutenticacionUsuario;
+import com.aluracursos.forohub.domain.usuario.Usuario;
+import com.aluracursos.forohub.infra.security.DatosTokenJWT;
+import com.aluracursos.forohub.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,14 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacionController {
 
     @Autowired
+    private TokenService tokenService;
+
+    @Autowired
     private AuthenticationManager manager;
 
     @PostMapping
     public ResponseEntity realizarLogin(@RequestBody @Valid DatosAutenticacionUsuario datos) {
-        var authenticationToken = new UsernamePasswordAuthenticationToken(datos.email(), datos.contrasena());
-        var usuarioAutenticado = manager.authenticate(authenticationToken);
+        try{
+            var authenticationToken = new UsernamePasswordAuthenticationToken(datos.email(), datos.contrasena());
+            var usuarioAutenticado = manager.authenticate(authenticationToken);
 
+            var tokenJWT = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
 
-        return ResponseEntity.ok().build();
+            return ResponseEntity.ok(new DatosTokenJWT(tokenJWT));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
 }
