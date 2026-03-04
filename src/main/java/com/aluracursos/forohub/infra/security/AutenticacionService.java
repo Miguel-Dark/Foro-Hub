18c36eed3abce5dbb0d0d34a82ea31f2cd2023c4
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-03-03 20:15:45 -0600

    feat: create AutenticacionService in infra.security package

---
+++ src/main/java/com/aluracursos/forohub/infra/security/AutenticacionService.java
@@ -0,0 +1,18 @@
+package com.aluracursos.forohub.infra.security;
+
+import com.aluracursos.forohub.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+public class AutenticacionService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username);
+    }
+}
