a39b4e28fbef202d6cc325fe2c08b7587566aa01
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-03-05 12:14:23 -0600

    docs: customize OpenAPI metadata and security schemes for ForoHub

---
+++ src/main/java/com/aluracursos/forohub/infra/springdoc/SpringDocConfiguration.java
@@ -0,0 +1,30 @@
+package com.aluracursos.forohub.infra.springdoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+
+public class SpringDocConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"))
+                )
+                .info(new Info()
+                        .title("ForoHub Chellenge - API Rest")
+                        .description("API Rest para el foro de dudas de Alura, con funcionalidades CRUD de tópicos, gestión de respuestas y control de acceso por roles (Estudiante e Instructor).")
+                        .contact(new Contact()
+                                .name("Miguel-Dark")
+                                .email("apache-dark@gmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://github.com/Miguel-Dark/Foro-Hub")));
+    }
+}
