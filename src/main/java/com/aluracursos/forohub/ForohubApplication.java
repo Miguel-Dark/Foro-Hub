83dfe3dcf504c81413b38f43ec599c2fa5bec8c7
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-03-05 11:36:36 -0600

    build: add springdoc-openapi-ui v2.6.0 and clean up main class

--- src/main/java/com/aluracursos/forohub/ForohubApplication.java
+++ src/main/java/com/aluracursos/forohub/ForohubApplication.java
@@ -1,10 +1,7 @@
 package com.aluracursos.forohub;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ForohubApplication {
@@ -13,10 +10,4 @@ public class ForohubApplication {
 		SpringApplication.run(ForohubApplication.class, args);
 	}
 
-//    @Bean
-//    public CommandLineRunner generator() {
-//        return args -> {
-//            System.out.println("HASH BCRYPT: " + new BCryptPasswordEncoder().encode("123456"));
-//        };
-//    }
 }
